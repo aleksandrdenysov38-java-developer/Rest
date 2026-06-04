6b84e01477cb51821f6d52cad3ed466b80f5b129
alex <aleksandrdenysov38@gmail.com>
2026-06-05 01:22:34 +0300

    Ready project

---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+public class Product {
+
+    private int id;
+    private String title;
+    private String description;
+    private double price;
+    private String brand;
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+}
